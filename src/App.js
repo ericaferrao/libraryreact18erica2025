02619e4aabb5283c551e2cf26533190e8034bcc9
Erica Stefny Ferrao <ferraoerica@gmail.com>
2025-11-27 06:56:31 -0800

    Add Books component and display library book list

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { BooksProvider } from './BooksContext';
 import Books from './components/Books';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BooksProvider>
-      <Books />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Books />} />
+        </Routes>
+      </Router>
     </BooksProvider>
-
   );
 }
 
