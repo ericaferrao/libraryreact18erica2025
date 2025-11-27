9da3c886217c9e9467e9f0cd74631647156e5103
Erica Stefny Ferrao <ferraoerica@gmail.com>
2025-11-27 08:02:41 -0800

    route-query-params-useLocation-URLSearchParams

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,8 +1,17 @@
 import { BooksContext } from "../BooksContext"
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Books() {
+    const query = new URLSearchParams(useLocation().search);
+    const search = query.get("search") || "";
     const books = useContext(BooksContext);
+
+    useEffect(() => {
+        //http://localhost:3000/?search=erica
+        console.log("query :", query);
+        console.log("Search query:", search);
+    }, [search]);
     return (
         <div>
             <h1>All Books</h1>
