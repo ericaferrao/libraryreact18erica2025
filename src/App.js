4dda79039a97feae1630633fb22cff7c5125822f
Erica Stefny Ferrao <ferraoerica@gmail.com>
2025-11-27 07:42:16 -0800

    404 page not found

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BooksProvider } from './BooksContext';
 import Books from './components/Books';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import BookDetails from './components/BookDetails';
+import NotFound from './components/NotFound';
+
 function App() {
   return (
     <BooksProvider>
@@ -12,6 +14,7 @@ function App() {
           <Route path="/" element={<Books />} />
           <Route path="/books" element={<Navigate to="/" />} />
           <Route path="/books/:bookId" element={<BookDetails />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </BooksProvider>
